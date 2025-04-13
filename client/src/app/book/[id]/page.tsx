e2f955218ce9f5303ed46571a99d263d1ca0c563
hmk2024 <hm.kim@moyoplan.com>
2025-04-13 23:23:16 +0900

    fix: page props build error

--- client/src/app/book/[id]/page.tsx
+++ client/src/app/book/[id]/page.tsx
@@ -2,15 +2,23 @@ import { getBook } from "@/entities/book/api/book";
 import BookDetailPage from "@/pages/book/ui/BookDetailPage";
 import { notFound } from "next/navigation";
 
-export default async function BookDetail({ params }: { params: { id: string } }) {
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
 
+export default async function Page({ params }: PageProps) {
   const { id } = await params;
 
   if (!id) {
-    notFound();
+    return notFound();
   }
 
   const book = await getBook(id);
+  if (!book) {
+    return notFound();
+  }
 
-  return <BookDetailPage bookId={book._id}/>
+  return <BookDetailPage bookId={book._id} />;
 }
--- client/src/app/book/page.tsx
+++ client/src/app/book/page.tsx
@@ -1,8 +1,13 @@
 import BookPage from "@/pages/book/BookPage";
 import Layout from "@/widgets/layout/ui/Layout";
 
+interface PageProps {
+  searchParams: Promise<{
+    page: string;
+  }>;
+}
 
-export default async function BookDetailPage({ searchParams }: { searchParams: { page: string } }) {
+export default async function BookDetailPage({ searchParams }: PageProps) {
 
   const params = await searchParams;
 
